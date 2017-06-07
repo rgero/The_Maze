935b5ce1b284caa28a5e52e0aaf852fb180789a2
Roy Gero <roy.gero.ii@gmail.com>
2017-06-07 19:29:05 -0400

    Adding a win collider to the End Block. Not sure how I want to show the end of the level yet. Maybe throw the player up in the air or something.

---
+++ Assets/Scripts/WinConditionCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinConditionCollider : MonoBehaviour {
+	void OnTriggerEnter(Collider collider){
+		GameObject collidedObject = collider.gameObject;
+		if (collidedObject.name == "Player") {
+			Debug.Log ("Player wins");
+		}
+	}
+}
