33b09c6120b16369a33a7e28e5472b4e23ce0d0b
Roy Gero <roy.gero.ii@gmail.com>
2017-06-08 14:47:19 -0400

    Adding a flashlight and turning off the directional light.

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+	
+	public GameObject characterObject;
+	Camera playerCamera;
+	Vector3 flashlightHeight;
+	float handOffset; // I don't want the light to appear eye level.
+
+	// Use this for initialization
+	void Awake () {
+		characterObject = GameObject.FindGameObjectWithTag ("MainCamera");
+		playerCamera = characterObject.GetComponent<Camera> ();
+		handOffset = characterObject.transform.localPosition.y / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		flashlightHeight = playerCamera.transform.position;
+		flashlightHeight.y -= handOffset;
+
+		this.transform.rotation = playerCamera.transform.rotation;
+		this.transform.position = flashlightHeight;
+	}
+}
