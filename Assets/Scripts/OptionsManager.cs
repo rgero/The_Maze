a5325096f39e788178162115a5a03e97f6c4b7dc
Roy Gero <roy.gero.ii@gmail.com>
2017-06-15 12:42:19 -0400

    Adding the Options Menu. Right now the menu is not dismissed when you hit save or cancel, but it will set the controller schemes to the PlayerPrefs.

---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour {
+
+	Dropdown playerOneDropdown;
+	Dropdown playerTwoDropdown;
+
+	// Use this for initialization
+	void Start () {
+		GameObject playerOneOptions = GameObject.Find ("P1_Dropdown");
+		GameObject playerTwoOptions = GameObject.Find ("P2_Dropdown");
+
+		playerOneDropdown = playerOneOptions.GetComponent<Dropdown> ();
+		playerTwoDropdown = playerTwoOptions.GetComponent<Dropdown> ();
+
+		//Restoring the player's preferences if they exist.
+		// These will return 0 if they are non-existent which is fine because that is keyboard.
+		int p1Control = PlayerPrefs.GetInt ("P1_Controller_Scheme");
+		int p2Control = PlayerPrefs.GetInt ("P2_Controller_Scheme");
+
+		playerOneDropdown.value = p1Control;
+		playerTwoDropdown.value = p2Control;
+	}
+
+	public void onClick(){
+
+		int p1Control = playerOneDropdown.value;
+		int p2Control = playerTwoDropdown.value;
+
+		PlayerPrefs.SetInt ("P1_Controller_Scheme", p1Control);
+		PlayerPrefs.SetInt ("P2_Controller_Scheme", p2Control);
+	}
+}
