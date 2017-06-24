3cdfd88cbc84fa8140f0726b90b66f426be14579
Roy Gero <roy.gero.ii@gmail.com>
2017-06-23 21:41:17 -0400

    Adding a winning display and set it up so that way I can change the text to the appropriate player. Right now only player one has a win condition.

--- Assets/Scripts/WinConditionCollider.cs
+++ Assets/Scripts/WinConditionCollider.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinConditionCollider : MonoBehaviour {
+
+	public GameObject winPrefab;
+	bool winTriggered = false;
+
+
 	void OnTriggerEnter(Collider collider){
 		GameObject collidedObject = collider.gameObject;
-		if (collidedObject.name == "Player") {
-			Debug.Log ("Player wins");
+		if (collidedObject.name == "Player" && !winTriggered) {
+			winTriggered = true;
+			GameObject winning = Instantiate (winPrefab) as GameObject;
+			Text winningText = GameObject.Find ("WinnerText").GetComponent<Text> ();
 		}
 	}
 }
