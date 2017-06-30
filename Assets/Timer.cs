d5ef8b4a7e4dcf8019f48d3bc7f992f0366fc8c3
Roy Gero <roy.gero.ii@gmail.com>
2017-06-29 20:27:16 -0400

    Adding the first implementation of the Game Timer. I figure this can be a separate object so that way I can use it in both the Single Player and Multiplayer modes.

---
+++ Assets/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Timer : MonoBehaviour {
+
+	private float countDownTimer;
+	GameObject playerObject;
+	FirstPersonController fpsController;
+	bool presentMessage;
+	public Text timeText;
+	public GameObject EndGameTextPrefab;
+
+	// Use this for initialization
+	void Start () {
+		presentMessage = false;
+		countDownTimer = GameConstants.TIME_LIMIT;
+		playerObject = GameObject.Find ("Player");
+		fpsController = playerObject.GetComponent<FirstPersonController> ();
+		timeText = GameObject.Find ("TimerText").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeText.text = string.Format ("Time Remaining: {0:0.00}", countDownTimer);
+		countDownTimer -= Time.deltaTime;
+		if (countDownTimer < 0) {
+			fpsController.enabled = false;
+			timeText.text = "GAME OVER!";
+
+			if (!presentMessage) {
+				presentMessage = true;
+				GameObject gameOver = Instantiate (EndGameTextPrefab) as GameObject;
+				Text winnerText = GameObject.Find ("WinnerText").GetComponent<Text> ();
+				winnerText.text = "Player 1 loses";
+			}
+		}
+		
+	}
+}
