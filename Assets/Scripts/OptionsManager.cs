7f4061ae500ebc0f81c73878023185803a0c3b48
Roy Gero <roy.gero.ii@gmail.com>
2017-09-24 20:42:09 -0400

    Starting to implement a system where the options for controllers that aren't there are hidden.

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -6,8 +6,13 @@ using XboxCtrlrInput;
 
 public class OptionsManager : MonoBehaviour {
 
+	Dropdown playerSelector;
 	Dropdown playerOneDropdown;
 	Dropdown playerTwoDropdown;
+	int numberOfPlayers;
+	int p1Control;
+	int p2Control;
+	List<Dropdown> listOfDrops;
 	Text errorText;
 
 	// Use this for initialization
@@ -17,22 +22,31 @@ public class OptionsManager : MonoBehaviour {
 		GameObject playerTwoOptions = GameObject.Find ("P2_Dropdown");
 		errorText = GameObject.Find ("ErrorText").GetComponent<Text> ();
 
+
+		playerSelector = GameObject.Find ("Players_Dropdown").GetComponent<Dropdown> ();
 		playerOneDropdown = playerOneOptions.GetComponent<Dropdown> ();
 		playerTwoDropdown = playerTwoOptions.GetComponent<Dropdown> ();
 
+		listOfDrops = new List<Dropdown> ();
+		listOfDrops.Add (playerOneDropdown);
+		listOfDrops.Add (playerTwoDropdown);
+
 		//Restoring the player's preferences if they exist.
 		// These will return 0 if they are non-existent which is fine because that is keyboard.
-		int p1Control = PlayerPrefs.GetInt ("P1_Controller_Scheme");
-		int p2Control = PlayerPrefs.GetInt ("P2_Controller_Scheme");
+		numberOfPlayers = PlayerPrefs.GetInt("NumberOfPlayers");
+		p1Control = PlayerPrefs.GetInt ("P1_Controller_Scheme");
+		p2Control = PlayerPrefs.GetInt ("P2_Controller_Scheme");
 
+		playerSelector.value = numberOfPlayers;
 		playerOneDropdown.value = p1Control;
 		playerTwoDropdown.value = p2Control;
 	}
 
 	public void onClick(){
 
-		int p1Control = playerOneDropdown.value;
-		int p2Control = playerTwoDropdown.value;
+		p1Control = playerOneDropdown.value;
+		p2Control = playerTwoDropdown.value;
+		numberOfPlayers = playerSelector.value;
 
 		bool status = true; // True means Ok.
 		string err_msg = "";
@@ -48,12 +62,23 @@ public class OptionsManager : MonoBehaviour {
 		if (status) {
 			PlayerPrefs.SetInt ("P1_Controller_Scheme", p1Control);
 			PlayerPrefs.SetInt ("P2_Controller_Scheme", p2Control);
+			PlayerPrefs.SetInt ("NumberOfPlayers", numberOfPlayers);
 
 			LevelManager levelManger = GameObject.Find ("LevelManager").GetComponent<LevelManager> ();
 			levelManger.LoadLevel ("StartMenu");
 		} else {
 			errorText.text = err_msg;
 		}
-			
 	}
+
+	public void changeNumberOfPlayers(){
+		int targetNumber = playerSelector.value;
+		for (int i = 0; i < targetNumber; i++) {
+			listOfDrops [i].Show ();
+		}
+		for (int i = targetNumber; i < listOfDrops.Count; i++) {
+			listOfDrops [i].Hide ();
+		}
+	}
+
 }
